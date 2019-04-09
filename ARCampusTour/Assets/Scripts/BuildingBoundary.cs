f09791386945c17b0ad11bee82a1bfd97df7055d
Rebecca Medina (RIT Student) <rcm3517@ad.rit.edu>
2019-04-09 14:29:08 -0400

    Colliding scripts

---
+++ ARCampusTour/Assets/Scripts/BuildingBoundary.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingBoundary : MonoBehaviour
+{
+    
+	public Vector3 boundMin;
+	public Vector3 boundMax;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+	/// <summary>
+	/// Gets the X minimum.
+	/// Retrieves the minimum X value (left edge) of this sprite
+	/// </summary>
+	/// <returns>The X minimum as a float.</returns>
+	public float GetXMin()
+	{
+		//Debug.Log ("Returning min x " + boundMin.x);
+		return boundMin.x;
+		//return gameObject.GetComponent<SpriteRenderer> ().sprite.bounds.min.x;
+	}
+
+	/// <summary>
+	/// Gets the X maximum.
+	/// Retrieves the maximum X value (right edge) of this sprite
+	/// </summary>
+	/// <returns>The X max as a float.</returns>
+	public float GetXMax()
+	{
+		//Debug.Log ("Returning max x " + boundMax.x);
+		return boundMax.x;
+		//return gameObject.GetComponent<SpriteRenderer> ().sprite.bounds.min.x;
+	}
+
+	/// <summary>
+	/// Gets the Y minimum.
+	/// Retrieves the minimum Y value (bottom edge) of this sprite
+	/// </summary>
+	/// <returns>The Y minimum as a float</returns>
+	public float GetYMin()
+	{
+		//Debug.Log ("Returning min x " + boundMin.x);
+		return boundMin.y;
+		//return gameObject.GetComponent<SpriteRenderer> ().sprite.bounds.min.x;
+	}
+
+	/// <summary>
+	/// Gets the Y maximum.
+	/// Retrieves the maximum Y value (top edge) of this sprite
+	/// </summary>
+	/// <returns>The Y maximum as a float</returns>
+	public float GetYMax()
+	{
+		//Debug.Log ("Returning max x " + boundMax.x);
+		return boundMax.y;
+		//return gameObject.GetComponent<SpriteRenderer> ().sprite.bounds.min.x;
+	}
+}
---
+++ ARCampusTour/Assets/Scripts/CollisionDetection.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDetection : MonoBehaviour
+{
+    GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    // Rather than storing 2 GameObjects as variables, they'll be passed in as arguments
+    bool CollidingObjectsAABB(GameObject a)
+    {
+        // Algorithm:  
+        // 1. Get the coordinates of both A and B
+        // 2. Compare their left, right, top and bottom edges
+        // 3. If colliding, return true
+
+        // Get left and right edges of first sprite
+        float aMinX = a.GetComponentInChildren<BuildingBoundary>().GetXMin();
+        float aMaxX = a.GetComponentInChildren<BuildingBoundary>().GetXMax();
+
+        // Get top and bottom edges of first sprite
+        float aMinY = a.GetComponentInChildren<BuildingBoundary>().GetYMin();
+        float aMaxY = a.GetComponentInChildren<BuildingBoundary>().GetYMax();
+
+        // Get left and right edges of second sprite
+        Vector3 playerPos = player.GetComponent<GpsTracking>().GetPosition();
+        float bX = playerPos.x;
+        float bY = playerPos.y;
+
+        // Check for a collision!
+        if (aMinX < bX && bX < aMaxX)
+        {
+            if (aMinY < bY && bY < aMaxY)
+            {
+                Debug.Log("Collision!");
+                return true;
+            }
+        }
+
+        return false;
+    }
+    
+}
