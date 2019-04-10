8f086158b313e4a7b8cc0b92e72033ae46abc2a6
Corinne Green <cmg7063@g.rit.edu>
2019-04-10 01:58:16 -0400

    Fix Main Menu, add touch to location blurb canvas
    location blurbs utilize LeanTouch asset, on touch of Show More, long blurb will be displayed and Show Less text appears, allowing the user to Show Less and revert to the short blurb

---
+++ ARCampusTour/Assets/Scripts/ShowMoreInfo.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowMoreInfo : MonoBehaviour
+{
+    private GameObject longBlurbDesc;
+    private GameObject shortBlurbDesc;
+    public Text showMore;
+
+    private int x;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        longBlurbDesc = GameObject.FindGameObjectWithTag("longBlurb");
+        shortBlurbDesc = GameObject.FindGameObjectWithTag("shortBlurb");
+
+        longBlurbDesc.SetActive(false);
+
+        x = 0;
+    }
+
+    void OnEnable()
+    {
+        Lean.Touch.LeanTouch.OnFingerTap += OnFingerTap;
+    }
+
+    void OnDisable()
+    {
+        Lean.Touch.LeanTouch.OnFingerTap -= OnFingerTap;
+    }
+
+    void OnFingerTap(Lean.Touch.LeanFinger finger)
+    {
+        Debug.Log("You just tapped the screen with finger " + finger.Index + " at " + finger.ScreenPosition);
+
+        if (x == 0)
+        {
+            longBlurbDesc.SetActive(true);
+            shortBlurbDesc.SetActive(false);
+            showMore.text = "Show Less";
+            x = 1;
+        }
+        else
+        {
+            longBlurbDesc.SetActive(false);
+            shortBlurbDesc.SetActive(true);
+            showMore.text = "Show More";
+            x = 0;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ ARCampusTour/Assets/Scripts/StreamVideo.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class StreamVideo : MonoBehaviour
+{
+    public RawImage rawImage;
+    public VideoPlayer videoPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(PlayVideo());
+    }
+
+    IEnumerator PlayVideo()
+    {
+        videoPlayer.Prepare();
+        WaitForSeconds waitForSeconds = new WaitForSeconds(1);
+        while(!videoPlayer.isPrepared)
+        {
+            yield return waitForSeconds;
+            break;
+        }
+        rawImage.texture = videoPlayer.texture;
+        videoPlayer.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
