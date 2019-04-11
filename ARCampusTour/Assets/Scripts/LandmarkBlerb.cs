d7f252f5fc86cc3a06751e3ce257619a8cec9d36
Rebecca Medina (RIT Student) <rcm3517@ad.rit.edu>
2019-04-11 18:45:48 -0400

    add landmark gameObjects and basic pathfollowing

---
+++ ARCampusTour/Assets/Scripts/LandmarkBlerb.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LandmarkBlerb : MonoBehaviour
+{
+    public string shortBlurb;
+    public string longBlurb;
+
+    private GameObject longBlurbDesc;
+    private GameObject shortBlurbDesc;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        longBlurbDesc = GameObject.FindGameObjectWithTag("longBlurb");
+        shortBlurbDesc = GameObject.FindGameObjectWithTag("shortBlurb");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void UpdateShortBlurb()
+    {
+        shortBlurbDesc.GetComponent<Text>().text = shortBlurb;
+    }
+    void UpdateLongBlurb()
+    {
+        longBlurbDesc.GetComponent<Text>().text = longBlurb;
+
+    }
+}
