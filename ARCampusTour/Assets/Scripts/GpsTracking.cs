c855addccab1c4ae9358a097c91592ac679a17e6
TomMakes <tcr4595@rit.edu>
2019-04-10 20:42:10 -0400

    Worked on the GPS file some more, it is near completed, however no testing has been done on it yet

--- ARCampusTour/Assets/Scripts/GpsTracking.cs
+++ ARCampusTour/Assets/Scripts/GpsTracking.cs
@@ -5,16 +5,11 @@ using UnityEngine;
 public class GpsTracking : MonoBehaviour
 {
     // Postition on Earth (in kilometers)
-    private float latitude;
-    private float longtidue;
+    public float latitude;
+    public float longtidue;
+    public float altitude;
     public bool updateLocation = true;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        latitude = 0f;
-        longtidue = 0f;
-    }
+
 
     IEnumerator StartUpdatingLocation()
     {
@@ -22,44 +17,60 @@ public class GpsTracking : MonoBehaviour
         if (!Input.location.isEnabledByUser)
             print("Throw Exception!, User does not have location enabled!");
 
-        // Start service before querying location
-        Input.location.Start();
-
-        // Wait until service initializes
-        int maxWait = 20;
-        print("Getting Location Data");
-        while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)
+        // Next, check if the user has the location query service already turned on
+        if (Input.location.status == LocationServiceStatus.Running)
         {
-            yield return new WaitForSeconds(1);
-            maxWait--;
-        }
-
-        // Service didn't initialize in 20 seconds
-        if (maxWait < 1)
-        {
-            print("Timed out");
-            yield break;
-        }
-
-        // Connection has failed
-        if (Input.location.status == LocationServiceStatus.Failed)
-        {
-            print("Unable to determine device location");
-            yield break;
+            latitude = Input.location.lastData.latitude;
+            longtidue = Input.location.lastData.longitude;
+            altitude = Input.location.lastData.altitude;
         }
         else
         {
-            // Access granted and location value could be retrieved
-            print("Location: " + Input.location.lastData.latitude + " " + Input.location.lastData.longitude + " " + Input.location.lastData.altitude + " " + Input.location.lastData.horizontalAccuracy + " " + Input.location.lastData.timestamp);
-        }
+            // Start service before querying location
+            Input.location.Start();
 
-        // Stop service if there is no need to query location updates continuously
-        Input.location.Stop();
+            // Wait until service initializes
+            int maxWait = 20;
+            print("Getting Location Data");
+            while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)
+            {
+                yield return new WaitForSeconds(1);
+                maxWait--;
+            }
+
+            // Service didn't initialize in 20 seconds
+            if (maxWait < 1)
+            {
+                print("Timed out");
+                yield break;
+            }
+
+            // Connection has failed
+            if (Input.location.status == LocationServiceStatus.Failed)
+            {
+                print("Unable to determine device location");
+                yield break;
+            }
+            else
+            {
+                // Access granted and location value could be retrieved
+                print("Location: " + Input.location.lastData.latitude + " " + Input.location.lastData.longitude + " " + Input.location.lastData.altitude + " " + Input.location.lastData.horizontalAccuracy + " " + Input.location.lastData.timestamp);
+            }
+        }
+    }
+
+        // Start is called before the first frame update
+        void Start()
+    {
+        latitude = 0f;
+        longtidue = 0f;
+        altitude = 0f;
     }
 
     void StopUpdatingLocation()
     {
-
+        // Stop service if there is no need to query location updates continuously
+        Input.location.Stop();
     }
 
     // Update is called once per frame
@@ -71,12 +82,12 @@ public class GpsTracking : MonoBehaviour
         }
         else
         {
-            StartUpdatingLocation();
+            StartCoroutine(StartUpdatingLocation());
         }
     }
 
     public Vector3 GetPosition()
     {
-        return new Vector3();
+        return new Vector3(latitude, longtidue, altitude);
     }
 }
