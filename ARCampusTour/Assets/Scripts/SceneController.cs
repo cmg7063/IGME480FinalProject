658c70b7e6e5169b2e16ad77956b7c4dde1f422c
Corinne Green <cmg7063@g.rit.edu>
2019-04-14 01:10:55 -0400

    Fix canvas resolution for portrait mode, add credits, directory, and about scenes. add button functionality

---
+++ ARCampusTour/Assets/Scripts/SceneController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SceneController : MonoBehaviour
+{
+    public Button startTour;
+    public Button directory;
+    public Button about;
+    public Button credits;
+    public Button quit;
+    public Button menu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void StartTour()
+    {
+        SceneManager.LoadScene("LocationBlurb");
+    }
+
+    public void Directory()
+    {
+        SceneManager.LoadScene("DirectoryList");
+    }
+
+    public void About()
+    {
+        SceneManager.LoadScene("About");
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void Menu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
