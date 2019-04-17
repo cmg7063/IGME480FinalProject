0d1cbefb7a8e8d5b7091d905ae3ae16cece4100c
Rebecca Medina <rcm3517@rit.edu>
2019-04-17 01:40:13 -0400

    academic locations

--- ARCampusTour/Assets/Scripts/LandmarkBlerb.cs
+++ ARCampusTour/Assets/Scripts/LandmarkBlerb.cs
@@ -9,7 +9,7 @@ public class LandmarkBlerb : MonoBehaviour
     public string longBlurb;
 
     public GameObject longBlurbDesc;
-    private GameObject shortBlurbDesc;
+    public GameObject shortBlurbDesc;
 
     // Start is called before the first frame update
     void Start()
