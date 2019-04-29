5901f7db84b10fa3e9fac175c7019ee6ce6a8dd4
TomMakes <tcr4595@rit.edu>
2019-04-29 09:41:20 -0400

    Made script to find angle to adjust to for arrow to point to next path location. Currently untested and most likely not complete

--- ARCampusTour/Assets/Scripts/ArrowDirection.cs
+++ ARCampusTour/Assets/Scripts/ArrowDirection.cs
@@ -5,8 +5,12 @@ using UnityEngine;
 public class ArrowDirection : MonoBehaviour
 {
     GameObject player;
+    Vector3 playerLoc;
     GameObject arrow;
     Vector3 target;
+    Vector3 northVector;
+
+    public float angleToTrack;
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +22,23 @@ public class ArrowDirection : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        playerLoc = player.GetComponent<GpsTracking>().GetPosition();
+        northVector = new Vector3(0, 0, 0);
+        // Get the location of the point to make up northVector
+        if (target.x > playerLoc.x)
+            northVector.x += 1;
+        else
+            northVector.x -= 1;
+
+        Vector3 locationVector = target - playerLoc;
+
+        // Get the angle between northVec and vector created by player location and target location
+        angleToTrack = Mathf.Acos(Vector3.Dot(northVector, locationVector) /
+            (Mathf.Sqrt((locationVector.x * locationVector.x) + (locationVector.z * locationVector.z))
+           * Mathf.Sqrt((northVector.x * northVector.x) + (northVector.z * northVector.z))));
+
+        print("The angle currently is " + angleToTrack);
+
         if (Input.location.status == LocationServiceStatus.Running)
             gameObject.transform.rotation = Quaternion.Euler(0, player.GetComponent<GpsTracking>().GetDirection(), 0);
     }
